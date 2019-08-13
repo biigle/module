0658a723b05499b9110f94008a5d4f33aed4ba4d
Martin Zurowietz <martin@zurowietz.de>
2019-08-13 13:43:25 +0200

    Initial commit

---
+++ src/ModuleServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Biigle\Modules\Module;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider {
+
+   /**
+   * Bootstrap the application events.
+   *
+   * @return  void
+   */
+   public function boot()
+   {
+      //
+   }
+
+   /**
+   * Register the service provider.
+   *
+   * @return  void
+   */
+   public function register()
+   {
+      //
+   }
+}
