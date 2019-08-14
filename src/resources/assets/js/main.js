071e5b28ad977a638272563ae0dc794920c09cd0
Martin Zurowietz <martin@zurowietz.de>
2019-08-14 13:33:41 +0200

    Add build system configuration

---
+++ src/resources/assets/js/main.js
@@ -0,0 +1,21 @@
+biigle.$viewModel('quotes-container', function (element) {
+    var messages = biigle.$require('messages.store');
+    new Vue({
+        el: element,
+        data: {
+            quote: '',
+        },
+        methods: {
+            refreshQuote: function () {
+                this.$http.get('quotes/new')
+                    .then(this.handleResponse, messages.handleErrorResponse)
+            },
+            handleResponse: function (response) {
+                this.quote = response.body;
+            },
+        },
+        created: function () {
+            this.refreshQuote();
+        },
+    });
+});
